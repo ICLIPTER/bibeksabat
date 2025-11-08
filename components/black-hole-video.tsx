bcf136723b986fd3dfdff44c2ac09fe9b6c19b4c
BIBEK SABAT <157093312+ICLIPTER@users.noreply.github.com>
2025-11-08 07:16:11 +0530

    Update black-hole-video.tsx

--- components/black-hole-video.tsx
+++ components/black-hole-video.tsx
@@ -7,24 +7,24 @@ import { motion } from "framer-motion";
 export function BlackHoleVideo() {
   const { theme } = useTheme();
   const [mounted, setMounted] = useState(false);
-
+  
   useEffect(() => {
     setMounted(true);
   }, []);
 
   if (!mounted) return null;
-  if (theme !== "dark") return null;
+  if (theme !== 'dark') return null;
 
   return (
-    <motion.div
+    <motion.div 
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
-      className="absolute inset-x-0 top-0 h-[700px] pointer-events-none z-[-2] overflow-hidden"
+      className="fixed inset-0 overflow-hidden z-[-2] pointer-events-none"
     >
       <video
-        src="/blackhole.mp4"
-        className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[700px] h-[700px] lg:w-full lg:h-[700px] object-cover opacity-70"
+        src="/blackhole.webm"
+        className="absolute rotate-180 top-[-340px] left-0 w-[700px] h-[670px] lg:w-full lg:h-[700px] object-cover overflow-hidden opacity-70"
         autoPlay
         loop
         muted
